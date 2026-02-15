e44a19394ed42e2c779b3be8847157b6d27e3e77
ElectroGamesDev <2cool4u567@gmail.com>
2026-02-14 19:59:10 -0600

    Citizens are now loaded asynchronously

--- src/main/java/com/electro/hycitizens/listeners/ChunkPreLoadListener.java
+++ src/main/java/com/electro/hycitizens/listeners/ChunkPreLoadListener.java
@@ -34,9 +34,12 @@ public class ChunkPreLoadListener {
 
     public void onChunkPreload(ChunkPreLoadProcessEvent event) {
         World world = event.getChunk().getWorld();
-        
+        long eventChunkIndex = event.getChunk().getIndex();
+        UUID worldUUID = world.getWorldConfig().getUuid();
+
+        // Collect citizens that belong to this chunk, then return from the event
         for (CitizenData citizen : plugin.getCitizensManager().getAllCitizens()) {
-            if (!world.getWorldConfig().getUuid().equals(citizen.getWorldUUID()))
+            if (!worldUUID.equals(citizen.getWorldUUID()))
                 continue;
 
             // Skip citizens that were just created (within last 10 seconds) to prevent double spawning
@@ -46,144 +49,151 @@ public class ChunkPreLoadListener {
             }
 
             // Check if the citizens is in the chunk
-            long citizenChunkIndex = ChunkUtil.indexChunkFromBlock((int)citizen.getPosition().x, (int)citizen.getPosition().getZ());
-            if (event.getChunk().getIndex() != citizenChunkIndex) {
+            long citizenChunkIndex = ChunkUtil.indexChunkFromBlock((int)citizen.getPosition().x, (int)citizen.getPosition().z);
+            if (eventChunkIndex != citizenChunkIndex) {
                 continue;
             }
 
-            // First check if the chunk is already loaded
+            // Hand off the heavy work to run outside the event
             long chunkIndex = ChunkUtil.indexChunkFromBlock(citizen.getPosition().x, citizen.getPosition().z);
-            WorldChunk loadedChunk = world.getChunkIfLoaded(chunkIndex);
-            if (loadedChunk != null) {
-                world.execute(() -> {
-                    Ref<EntityStore> entityRef = null;
-                    if (citizen.getSpawnedUUID() != null) {
-                        entityRef = world.getEntityRef(citizen.getSpawnedUUID());
+            HytaleServer.SCHEDULED_EXECUTOR.schedule(() -> {
+                processCitizenAsync(world, citizen, chunkIndex);
+            }, 0, TimeUnit.MILLISECONDS);
+        }
+    }
+
+    private void processCitizenAsync(World world, CitizenData citizen, long chunkIndex) {
+        // First check if the chunk is already loaded
+        WorldChunk loadedChunk = world.getChunkIfLoaded(chunkIndex);
+        if (loadedChunk != null) {
+            world.execute(() -> {
+                Ref<EntityStore> entityRef = null;
+                if (citizen.getSpawnedUUID() != null) {
+                    entityRef = world.getEntityRef(citizen.getSpawnedUUID());
+                }
+
+                if (entityRef == null) {
+                    plugin.getCitizensManager().spawnCitizenNPC(citizen, true);
+                } else {
+                    // Entity exists, update skin
+                    if (citizen.isPlayerModel()) {
+                        plugin.getCitizensManager().updateCitizenSkin(citizen, true);
                     }
 
-                    if (entityRef == null) {
-                        plugin.getCitizensManager().spawnCitizenNPC(citizen, true);
-                    } else {
-                        // Entity exists, update skin
-                        if (citizen.isPlayerModel()) {
-                            plugin.getCitizensManager().updateCitizenSkin(citizen, true);
-                        }
+                    // Update NPC ref
+                    citizen.setNpcRef(entityRef);
+                }
+            });
 
-                        // Update NPC ref
-                        citizen.setNpcRef(entityRef);
-                    }
-                });
+            // Schedule delayed hologram check for chunk already loaded case
+            scheduleHologramCheck(world, citizen, chunkIndex);
 
-                // Schedule delayed hologram check for chunk already loaded case
-                scheduleHologramCheck(world, citizen, chunkIndex);
+            return;
+        }
 
-                continue;
+        // Chunk is not loaded. Try to wait for it to load, if it takes too long, assume it won't load and load it
+        long start = System.currentTimeMillis();
+        final ScheduledFuture<?>[] futureRef = new ScheduledFuture<?>[1];
+        boolean[] spawned = { false };
+        boolean[] queued = { false };
+        boolean[] hologramCheckScheduled = { false };
+
+        futureRef[0] = HytaleServer.SCHEDULED_EXECUTOR.scheduleAtFixedRate(() -> {
+            if (spawned[0]) {
+                futureRef[0].cancel(false);
+                return;
             }
 
-            // Chunk is not loaded. Try to wait for it to load, if it takes too long, assume it won't load and load it
-            long start = System.currentTimeMillis();
-            final ScheduledFuture<?>[] futureRef = new ScheduledFuture<?>[1];
-            boolean[] spawned = { false };
-            boolean[] queued = { false };
-            boolean[] hologramCheckScheduled = { false };
+            // Timeout
+            long elapsedMs = System.currentTimeMillis() - start;
+            WorldChunk loadedChunk2 = world.getChunkIfLoaded(chunkIndex);
 
-            futureRef[0] = HytaleServer.SCHEDULED_EXECUTOR.scheduleAtFixedRate(() -> {
-                if (spawned[0]) {
-                    futureRef[0].cancel(false);
-                    return;
-                }
+            if (elapsedMs >= 15_000 || loadedChunk2 != null) {
+                futureRef[0].cancel(false);
 
-                // Timeout
-                long elapsedMs = System.currentTimeMillis() - start;
-                WorldChunk loadedChunk2 = world.getChunkIfLoaded(chunkIndex);
-
-                if (elapsedMs >= 15_000 || loadedChunk2 != null) {
-                    futureRef[0].cancel(false);
-
-                    // Check if the citizen spawned, if it didn't then it's likely it's in an unloaded chunk. Load the chunk and try again
-                    // Todo: This isn't very performant if there's a lot of citizens in unloaded chunks
-                    if (!spawned[0]) {
-                        WorldChunk chunkInMemory = world.getChunkIfInMemory(chunkIndex);
-                        if (chunkInMemory == null) {
-                            // Chunk is not in memory, there's nothing we can do to check if citizen is loaded or not
-                            return;
-                        }
-
-                        world.loadChunkIfInMemory(chunkIndex);
-
-                        world.execute(() -> {
-                            Ref<EntityStore> entityRef = null;
-                            if (citizen.getSpawnedUUID() != null) {
-                                entityRef = world.getEntityRef(citizen.getSpawnedUUID());
-                            }
-
-                            // If the chunk loads, try to spawn the citizen if it doesn't exist
-                            if (entityRef == null) {
-                                plugin.getCitizensManager().spawnCitizenNPC(citizen, true);
-                            } else {
-                                // Entity exists, update skin
-                                if (citizen.isPlayerModel()) {
-                                    plugin.getCitizensManager().updateCitizenSkin(citizen, true);
-                                }
-
-                                // Update NPC ref
-                                citizen.setNpcRef(entityRef);
-                            }
-                        });
-
-                        // Schedule delayed hologram check after timeout spawn
-                        if (!hologramCheckScheduled[0]) {
-                            hologramCheckScheduled[0] = true;
-                            scheduleHologramCheck(world, citizen, chunkIndex);
-                        }
-                    }
-
-                    return;
-                }
-
-                if (queued[0]) {
-                    return;
-                }
-                queued[0] = true;
-
-                world.execute(() -> {
-                    WorldChunk chunk = world.getChunkIfLoaded(chunkIndex);
-
-                    if (chunk == null) {
-                        queued[0] = false;
+                // Check if the citizen spawned, if it didn't then it's likely it's in an unloaded chunk. Load the chunk and try again
+                // Todo: This isn't very performant if there's a lot of citizens in unloaded chunks
+                if (!spawned[0]) {
+                    WorldChunk chunkInMemory = world.getChunkIfInMemory(chunkIndex);
+                    if (chunkInMemory == null) {
+                        // Chunk is not in memory, there's nothing we can do to check if citizen is loaded or not
                         return;
                     }
 
-                    spawned[0] = true;
-                    futureRef[0].cancel(false);
+                    world.loadChunkIfInMemory(chunkIndex);
 
-                    Ref<EntityStore> entityRef = null;
-                    if (citizen.getSpawnedUUID() != null) {
-                        entityRef = world.getEntityRef(citizen.getSpawnedUUID());
-                    }
-
-                    // If the chunk is loaded, try to spawn the citizen if it doesn't exist
-                    if (entityRef == null) {
-                        plugin.getCitizensManager().spawnCitizenNPC(citizen, true);
-                    } else {
-                        // Entity exists, update skin
-                        if (citizen.isPlayerModel()) {
-                            plugin.getCitizensManager().updateCitizenSkin(citizen, true);
+                    world.execute(() -> {
+                        Ref<EntityStore> entityRef = null;
+                        if (citizen.getSpawnedUUID() != null) {
+                            entityRef = world.getEntityRef(citizen.getSpawnedUUID());
                         }
 
-                        // Update NPC ref
-                        citizen.setNpcRef(entityRef);
-                    }
+                        // If the chunk loads, try to spawn the citizen if it doesn't exist
+                        if (entityRef == null) {
+                            plugin.getCitizensManager().spawnCitizenNPC(citizen, true);
+                        } else {
+                            // Entity exists, update skin
+                            if (citizen.isPlayerModel()) {
+                                plugin.getCitizensManager().updateCitizenSkin(citizen, true);
+                            }
 
-                    // Schedule delayed hologram check after periodic spawn
+                            // Update NPC ref
+                            citizen.setNpcRef(entityRef);
+                        }
+                    });
+
+                    // Schedule delayed hologram check after timeout spawn
                     if (!hologramCheckScheduled[0]) {
                         hologramCheckScheduled[0] = true;
                         scheduleHologramCheck(world, citizen, chunkIndex);
                     }
-                });
+                }
 
-            }, 0, 250, TimeUnit.MILLISECONDS);
-        }
+                return;
+            }
+
+            if (queued[0]) {
+                return;
+            }
+            queued[0] = true;
+
+            world.execute(() -> {
+                WorldChunk chunk = world.getChunkIfLoaded(chunkIndex);
+
+                if (chunk == null) {
+                    queued[0] = false;
+                    return;
+                }
+
+                spawned[0] = true;
+                futureRef[0].cancel(false);
+
+                Ref<EntityStore> entityRef = null;
+                if (citizen.getSpawnedUUID() != null) {
+                    entityRef = world.getEntityRef(citizen.getSpawnedUUID());
+                }
+
+                // If the chunk is loaded, try to spawn the citizen if it doesn't exist
+                if (entityRef == null) {
+                    plugin.getCitizensManager().spawnCitizenNPC(citizen, true);
+                } else {
+                    // Entity exists, update skin
+                    if (citizen.isPlayerModel()) {
+                        plugin.getCitizensManager().updateCitizenSkin(citizen, true);
+                    }
+
+                    // Update NPC ref
+                    citizen.setNpcRef(entityRef);
+                }
+
+                // Schedule delayed hologram check after periodic spawn
+                if (!hologramCheckScheduled[0]) {
+                    hologramCheckScheduled[0] = true;
+                    scheduleHologramCheck(world, citizen, chunkIndex);
+                }
+            });
+
+        }, 0, 250, TimeUnit.MILLISECONDS);
     }
 
     private void scheduleHologramCheck(World world, CitizenData citizen, long chunkIndex) {
